a3fda7443bdc0f6784c8253226f0da3ad491ea3c
Derian <dmrodriguez2000@gmail.com>
2024-11-26 13:07:35 -0600

    feat: get todos with pagination

--- admin-todos/app/api/seed/route.ts
+++ admin-todos/app/api/seed/route.ts
@@ -6,12 +6,27 @@ export async function GET(request: Request) {
 
   await prisma.todo.createMany({
     data: [
-      { description: 'Piedra del alma', complete: true },
-      { description: 'Piedra del poder' },
-      { description: 'Piedra del tiempo' },
-      { description: 'Piedra del espacio' },
-      { description: 'Piedra del realidad' },
-    ]
+      { description: 'Collect Infinity Stones', complete: true },
+      { description: 'Write documentation for the project' },
+      { description: 'Fix the login page bug' },
+      { description: 'Prepare presentation slides' },
+      { description: 'Learn GraphQL basics' },
+      { description: 'Fetch water from the river', complete: true },
+      { description: 'Build a house in Minecraft' },
+      { description: 'Learn TypeScript', complete: true },
+      { description: 'Finish work project' },
+      { description: 'Exercise in the morning' },
+      { description: 'Read a book on productivity', complete: true },
+      { description: 'Organize the desk', complete: true },
+      { description: 'Practice design patterns' },
+      { description: 'Write in my journal' },
+      { description: 'Update portfolio', complete: true },
+      { description: 'Cook a new recipe' },
+      { description: 'Set up CI/CD for a project', complete: true },
+      { description: 'Create an API with Prisma and Next.js' },
+      { description: 'Explore new technologies' },
+      { description: 'Review and close issues on GitHub', complete: true },
+    ],
   });
 
   return NextResponse.json({ message: 'Seed Executed!' });
