20cbf3a9f6119df9adc95ed8b2d18b50ac63a71f
Derian <dmrodriguez2000@gmail.com>
2025-01-03 14:15:37 -0600

    feat: update todo

--- admin-todos/components/todos/todos-grid.tsx
+++ admin-todos/components/todos/todos-grid.tsx
@@ -3,16 +3,26 @@
 import { Todo } from "@prisma/client"
 import { TodoItem } from "./todo-item";
 
+import * as api from "@/helpers/todos";
+import { useRouter } from "next/navigation";
+
 interface Props {
   todos?: Todo[];
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
+  const router = useRouter();
+
+  const toggleTodo = async(id: string, complete: boolean) => {
+    await api.updateTodo( id, complete );
+
+    router.refresh();
+  }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {
         todos.map( todo => (
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
         ))
       }
     </div>
