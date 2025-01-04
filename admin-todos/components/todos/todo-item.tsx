c1e73157a5ca140efab1aa85b6eb8aa552354796
Derian <dmrodriguez2000@gmail.com>
2025-01-03 21:28:37 -0600

    refactor: useOptimistic hook in todo item

--- admin-todos/components/todos/todo-item.tsx
+++ admin-todos/components/todos/todo-item.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { startTransition, useOptimistic } from "react";
 import { Todo } from "@prisma/client";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
@@ -9,20 +12,36 @@ interface Props {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
+  const [ todoOptimistic, toggleTodoOptimistic ] = useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean ) => ({ ...state, complete: newCompleteValue })
+  );
+
+  const onToggleTodo = async() => {
+    try {
+      startTransition( () => toggleTodoOptimistic( !todoOptimistic.complete ) );
+      
+      await toggleTodo( todoOptimistic.id, !todoOptimistic.complete );
+    } catch (error) {
+      startTransition( () => toggleTodoOptimistic( !todoOptimistic.complete ) );
+    }
+  }    
+
   return (
-    <article className={ todo.complete ? styles.todoDone : styles.todoPending }>
+    <article className={ todoOptimistic.complete ? styles.todoDone : styles.todoPending }>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div 
-          onClick={ () => toggleTodo(todo.id, !todo.complete) }
+          // onClick={ () => toggleTodo(todoOptimistic.id, !todoOptimistic.complete) }
+          onClick={ onToggleTodo }
           className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 ${ todo.complete ? 'bg-blue-100' : 'bg-red-100' }`}
         >
           {
-            todo.complete
+            todoOptimistic.complete
               ? <IoCheckboxOutline size={30} />
               : <IoSquareOutline size={30} />
           }
         </div>
-        <p className="text-center sm:text-left">{ todo.description }</p>
+        <p className="text-center sm:text-left">{ todoOptimistic.description }</p>
       </div>
     </article>
   )
