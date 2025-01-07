7ca77acf4d7f06bd3a9f59c2c28df75a01a30540
Derian <dmrodriguez2000@gmail.com>
2025-01-06 19:41:51 -0600

    feat: get selectedTab of the cookies

--- admin-todos/app/dashboard/cookies/page.tsx
+++ admin-todos/app/dashboard/cookies/page.tsx
@@ -1,16 +1,21 @@
 import { Metadata } from "next";
+import { cookies } from "next/headers";
+
 import { TabBar } from "@/components/tab-bar";
 
 export const metadata: Metadata = {
   title: 'Cookies Page',
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const cookieTab = Number(cookieStore.get('selectedTab')?.value ?? '1');
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <h1 className="text-3xl">Tabs</h1>
-        <TabBar />
+        <TabBar currentTab={ cookieTab } />
       </div>
     </div>
   );
