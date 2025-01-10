2e982902cb00a467630bf5b90be7e866255408d3
Derian <dmrodriguez2000@gmail.com>
2025-01-10 16:16:07 -0600

    feat: save product from cart in cookies

--- admin-todos/components/products/product-card.tsx
+++ admin-todos/components/products/product-card.tsx
@@ -1,66 +1,79 @@
-import Image from "next/image"
-import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5"
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
+
 import { Star } from "./star";
+import { addProductToCart } from "@/actions/shopping-cart-actions";
 
 interface Props {
-  id    : string;
-  name  : string;
-  price : number;
+  id: string;
+  name: string;
+  price: number;
   rating: number;
-  image : string;
+  image: string;
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
+  const router = useRouter();
+
+  const onAddToCart = () => {
+    addProductToCart(id);
+    router.refresh();
+  }
+
   return (
     <div className="shadow rounded-lg max-w-sm bg-gray-800 border-gray-100">
-      
+
       {/* Product Image */}
       <div className="p-2">
         <Image
-            width={500}
-            height={500}
-            className="rounded" 
-            src={ image }
-            alt={ name } />
+          width={500}
+          height={500}
+          className="rounded"
+          src={image}
+          alt={name} />
       </div>
-      
+
       {/* Title */}
       <div className="px-5 pb-5">
         <a href="#">
-          <h3 className="font-semibold text-xl tracking-tight text-white">{ name }</h3>
+          <h3 className="font-semibold text-xl tracking-tight text-white">{name}</h3>
         </a>
         <div className="flex items-center mt-2.5 mb-5">
-          
+
 
           {/* Stars */}
           {
-            Array(rating).fill(0).map( (x, i) => (
+            Array(rating).fill(0).map((x, i) => (
               <Star key={i} />
             ))
           }
 
           {/* Rating Number */}
           <span className="text-xs font-semibold mr-2 px-2.5 py-0.5 rounded bg-blue-200 text-blue-800 ml-3">
-            { rating.toFixed(2) }
+            {rating.toFixed(2)}
           </span>
         </div>
 
 
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-white">${ price }</span>
-          
+          <span className="text-3xl font-bold text-white">${price}</span>
+
           <div className="flex">
             <button
+              onClick={ onAddToCart }
               className="text-white mr-2 focus:ring-4ont-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
-                <IoAddCircleOutline size={25} />
+              <IoAddCircleOutline size={25} />
             </button>
             <button
               className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 hover:bg-red-700 focus:ring-red-800">
-                <IoTrashOutline size={20} />
+              <IoTrashOutline size={20} />
             </button>
           </div>
-          
+
         </div>
 
 
