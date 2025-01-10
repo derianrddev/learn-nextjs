cb57cf6703c99b5cbd623614bcaf3776917f7034
Derian <dmrodriguez2000@gmail.com>
2025-01-10 15:38:06 -0600

    feat: show products

--- admin-todos/app/dashboard/products/page.tsx
+++ admin-todos/app/dashboard/products/page.tsx
@@ -1,9 +1,14 @@
 import { ProductCard } from "@/components/products/product-card";
+import { products } from "@/data/products";
 
 export default function ProductsPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
-      <ProductCard />
+      {
+        products.map((product) => (
+          <ProductCard key={ product.id } { ...product } />
+        ))
+      }
     </div>
   )
 }
